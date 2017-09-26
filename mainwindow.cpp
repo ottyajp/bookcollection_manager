9cbc32888b67f70666c58cd82d6173789a99340a
ottyajp <ottyajp@gmail.com>
2017-09-26 22:55:52 +0900

    initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,42 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <string>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    view = new QWebEngineView(this);
+    ui->layout->addWidget(view);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    std::system("timeout 2 cat /dev/ttyACM0 > out.txt");
+    std::ifstream fs("./out.txt");
+    std::string str;
+    std::getline(fs, str);
+    ui->isbn->setText(QString::fromStdString(str));
+    QString isbn13 = QString::fromStdString(str).replace(QRegularExpression("\r"),"");
+    this->view->setUrl("https://www.amazon.co.jp/dp/product/"+isbn13to10(isbn13));
+    qDebug()<<view->url();
+    fs.close();
+}
+
+QString isbn13to10(QString isbn13){
+    QString isbn10 = isbn13.replace(0,3,"");
+    int checkdigit = 0;
+    for(int i=10; i>1; i--){
+        checkdigit += i * isbn10.at(10 - i).digitValue();
+    }
+    checkdigit = 11 - checkdigit%11;
+    isbn10.replace(9,1,QString::number(checkdigit));
+    qDebug()<<isbn10;
+    return isbn10;
+}
---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QHBoxLayout>
+#include <QtWebEngineWidgets/QWebEngineView>
+#include <cstdlib>
+#include <fstream>
+#include <QDebug>
+
+QString isbn13to10(QString isbn13);
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QWebEngineView *view;
+};
+
+#endif // MAINWINDOW_H
